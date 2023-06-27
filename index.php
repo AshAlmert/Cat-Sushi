753f4d88b3e24d878d17bb4c564918f6c384236d
Ash Almert <fast56511@gmail.com>
2023-06-27 23:07:07 +0300

    add image and css

--- index.php
+++ index.php
@@ -4,22 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./asset/css/main.css">
+    <style>
+    <?php include 'asset\css\main.css'; ?>
+    </style>
     <title>Cat Sushi | Главная страница</title>
 </head>
 <body>
     <div class="content">
         <header class="header">
             <div class="navigation">
+                <p class="logo-p">Cat Sushi</p>
+                <p class="p-location">Москва RU</p>
                 <a href="" class="link-nav">Главная</a>
                 <a href="" class="link-nav">Доставка</a>
                 <a href="" class="link-nav">О нас</a>
                 <a href="" class="link-nav">Новости</a>
                 <p class="p-phone">+7(945)-909-99-99</p>
-                <button class="nav-button"><img src="" alt="" class="but-image"></button>
-                <button class="nav-button"><img src="" alt="" class="but-image"></button>
-                <button class="nav-button"><img src="" alt="" class="but-image"></button>
-                <button class="nav-button">Корзина <img src="" alt="" class="but-image"></button>
+                <button class="nav-button"><img src="./asset/img/notification-bell.png" alt="bell" class="but-image"></button>
+                <button class="nav-button"><img src="./asset/img/heart.png" alt="heart" class="but-image"></button>
+                <button class="nav-button"><img src="./asset/img/user.png" alt="user" class="but-image"></button>
+                <button class="nav-button">Корзина <img src="./asset/img/cart.png" alt="cart" class="but-image"></button>
             </div>
         </header>
     </div>
