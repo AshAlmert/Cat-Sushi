28e26a4acb07bcc2f78fa6454f9716df50b38dd7
Ash Almert <fast56511@gmail.com>
2023-06-27 22:28:52 +0300

    Create navigation

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/css/main.css">
     <title>Cat Sushi | Главная страница</title>
 </head>
 <body>
-    
+    <div class="content">
+        <header class="header">
+            <div class="navigation">
+                <a href="" class="link-nav">Главная</a>
+                <a href="" class="link-nav">Доставка</a>
+                <a href="" class="link-nav">О нас</a>
+                <a href="" class="link-nav">Новости</a>
+                <p class="p-phone">+7(945)-909-99-99</p>
+                <button class="nav-button"><img src="" alt="" class="but-image"></button>
+                <button class="nav-button"><img src="" alt="" class="but-image"></button>
+                <button class="nav-button"><img src="" alt="" class="but-image"></button>
+                <button class="nav-button">Корзина <img src="" alt="" class="but-image"></button>
+            </div>
+        </header>
+    </div>
 </body>
 </html>
